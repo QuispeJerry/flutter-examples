b2ad0768851031ccf07ee87d167078e38b231756
JerryQG <u20201c043@upc.edu.pe>
2024-06-05 01:37:12 -0500

    se arreglo el problema de la linea amarilla

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,17 @@ class contenedor_ventanas extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(5.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
           children: [
             buildCard(context, 'https://www.shutterstock.com/image-illustration/view-waterfall-forest-600nw-2327645439.jpg', 'Ventana 1', 'screen1'),
+            const SizedBox(width: 5),
             buildCard(context, 'https://www.shutterstock.com/shutterstock/photos/2198245029/display_1500/stock-photo-autumn-nature-landscape-lake-bridge-in-fall-forest-path-way-in-gold-woods-romantic-view-image-2198245029.jpg', 'Ventana 2', 'screen2'),
+            const SizedBox(width: 5),
             buildCard(context, 'https://st5.depositphotos.com/64145070/64693/i/450/depositphotos_646930840-stock-photo-sunset-ocean-beach-beautiful-seascape.jpg', 'Ventana 3', 'screen3'),
           ],
+        ),
         ),
       ),
     );
