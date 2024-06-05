9b0dbce950100cfab077cdb6e93fdf7512810f40
JerryQG <u20201c043@upc.edu.pe>
2024-06-05 02:22:56 -0500

    se agrego la capa de opacidad en todo el card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//actual
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,44 +32,71 @@ class contenedor_ventanas extends StatelessWidget {
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
-          children: [
-            buildCard(context, 'https://www.shutterstock.com/image-illustration/view-waterfall-forest-600nw-2327645439.jpg', 'Ventana 1', 'screen1'),
-            const SizedBox(width: 5),
-            buildCard(context, 'https://www.shutterstock.com/shutterstock/photos/2198245029/display_1500/stock-photo-autumn-nature-landscape-lake-bridge-in-fall-forest-path-way-in-gold-woods-romantic-view-image-2198245029.jpg', 'Ventana 2', 'screen2'),
-            const SizedBox(width: 5),
-            buildCard(context, 'https://st5.depositphotos.com/64145070/64693/i/450/depositphotos_646930840-stock-photo-sunset-ocean-beach-beautiful-seascape.jpg', 'Ventana 3', 'screen3'),
-          ],
-        ),
+            children: [
+              buildCard(
+                  context,
+                  'https://www.shutterstock.com/image-illustration/view-waterfall-forest-600nw-2327645439.jpg',
+                  'Ventana 1',
+                  'screen1'),
+              const SizedBox(width: 5),
+              buildCard(
+                  context,
+                  'https://www.shutterstock.com/shutterstock/photos/2198245029/display_1500/stock-photo-autumn-nature-landscape-lake-bridge-in-fall-forest-path-way-in-gold-woods-romantic-view-image-2198245029.jpg',
+                  'Ventana 2',
+                  'screen2'),
+              const SizedBox(width: 5),
+              buildCard(
+                  context,
+                  'https://st5.depositphotos.com/64145070/64693/i/450/depositphotos_646930840-stock-photo-sunset-ocean-beach-beautiful-seascape.jpg',
+                  'Ventana 3',
+                  'screen3'),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-
-Widget buildCard(BuildContext context, String imagenCard, String texto, String ruta){
+Widget buildCard(
+    BuildContext context, String imagenCard, String texto, String ruta) {
   return GestureDetector(
     onTap: () {
       Navigator.pushNamed(context, ruta);
     },
     child: Card(
-      elevation: 5,
-      child: Container(
-        width: MediaQuery.of(context).size.width /3 - 10,
-        height: 200,
-        child: Stack(
-          children: [
-            Positioned.fill(child: Image.network(imagenCard, fit: BoxFit.cover,)),
-            Center(child: Text(texto, style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold, backgroundColor: Colors.black54,),textAlign: TextAlign.center,),)
-          ],
-        ),
-      )
-    ),
+        elevation: 5,
+        child: Container(
+          width: MediaQuery.of(context).size.width / 3 - 10,
+          height: 200,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                  child: Image.network(
+                imagenCard,
+                fit: BoxFit.cover,
+              )),
+              //capa de opacidad
+              Positioned.fill(
+                  child: Container(
+                color: Colors.black.withOpacity(0.6), // Capa negra con opacidad
+              )),
+              Center(
+                child: Text(
+                  texto,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        )),
   );
 }
-
-
-
 
 class card1 extends StatefulWidget {
   const card1({super.key});
